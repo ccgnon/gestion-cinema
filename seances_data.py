2acc114e937ebfa73a0d599e3c91f32dcea802df
Jeremie <nabbycarter@gmail.com>
2023-06-16 18:15:59 +0100

    gestion des seances

---
+++ seances_data.py
@@ -0,0 +1,20 @@
+SEANCES = [
+    {
+        "id": 1,
+        "label": "Foudre blanche",
+        "source": "Foudre blanche src",
+        "url": "https://youtu.be/ss7DMZ7Vahk",
+    },
+    {
+        "id": 2,
+        "label": "Terminator",
+        "source": "Terminator src",
+        "url": "https://youtu.be/eseaExBopxY",
+    },
+    {
+        "id": 3,
+        "label": "Mercenaires",
+        "source": "Mercenaires src",
+        "url": "https://www.youtube.com/watch?v=kwb7KneK2E8",  # noqa
+    },
+]
